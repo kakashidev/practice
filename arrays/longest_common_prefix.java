51927047500cee3b409bd3395963d4297ce0225a
devang <devanggupta008@gmail.com>
2022-06-18 12:59:47 +0530

    practice

---
+++ arrays/longest_common_prefix.java
@@ -0,0 +1,47 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int n = Integer.parseInt(read.readLine().trim());
+            String arr[] = read.readLine().trim().split(" ");
+
+            Solution ob = new Solution();
+            System.out.println(ob.longestCommonPrefix(arr, n));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    String longestCommonPrefix(String arr[], int n){
+        // code here
+        String result = arr[0];
+        for(int i=1;i<n;i++){
+            for(int j=0;j<Math.min(result.length(),arr[i].length());j++){
+                if(arr[i].charAt(j) != result.charAt(j)){
+                    result = result.substring(0,j);
+                    if(j==0){
+                        return "-1";
+                    }
+                    break;
+                }
+                if(j==Math.min(result.length(),arr[i].length())-1){
+                    result = result.substring(0,j+1);
+                }
+            }
+        }
+        return result;
+    }
+}
